03f88d214615066bb10c0e3f735f06d901c3412b
Jeremy Burns <j@jaburns.net>
2017-05-18 20:59:27 -0600

    Randomizing scale

--- src/world.cpp
+++ src/world.cpp
@@ -1,26 +1,14 @@
 #include "world.hpp"
 
 #include <glm/gtc/constants.hpp>
+#include <glm/gtc/random.hpp>
 #include <math.h>
-#include <cstdlib>
 
 World::World()
 {
     camera_position = { 0.0f, 0.0f, 0.0f };
     camera_up = { 0.0f, 1.0f, 0.0f };
     camera_look = { 0.0f, 0.0f, -1.0f };
-}
-
-static float rand_range(float lower, float upper)
-{
-    float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-    return lower + r * (upper - lower);
-}
-
-static glm::vec3 rand_normal()
-{
-    glm::vec3 v = { rand_range(-1,1), rand_range(-1,1), rand_range(-1,1) };
-    return glm::normalize(v);
 }
 
 World World::step(InputState& input) const
@@ -32,9 +20,9 @@ World World::step(InputState& input) const
 
     Teapot ntp;
     ntp.transform.position.z = -3.0f;
-    ntp.transform.scale = glm::vec3(0.01f);
-    ntp.velocity.position = 0.1f * rand_normal();
-    ntp.velocity.rotation = glm::rotate(glm::quat(), rand_range(-0.2f, 0.2f), rand_normal());
+    ntp.transform.scale = glm::vec3(glm::linearRand(0.005f, 0.01f));
+    ntp.velocity.position = glm::sphericalRand(0.1f);
+    ntp.velocity.rotation = glm::rotate(glm::quat(), glm::linearRand(-0.2f, 0.2f), glm::sphericalRand(1.0f));
     world.teapots.push_back(ntp);
 
     for (auto& tp : world.teapots) {
