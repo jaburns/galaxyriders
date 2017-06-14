d8c69e40fab25c0ff24670c1a3194a91fcdb5950
Jeremy Burns <j@jaburns.net>
2017-06-14 14:56:51 -0600

    Cleaned up main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 #include <chrono>
-#include <thread>
 #include "readinput.hpp"
 #include "render.hpp"
 #include "../shared/network.hpp"
 
-void render_from_server()
+static const int MAX_BUFFER_LEN = 8192;
+static const int PORT = 12345;
+
+int main(int argc, char** argv)
 {
     Renderer renderer;
-    World last_world, new_world;
-    float world_lerp = 0.0f;
     Input::bind_handlers(renderer.raw_glfw_window());
-
-    const int MAX_BUFFER_LEN = 8192;
-    const int PORT = 12345;
 
     UDPSocket socket;
     SocketAddress send_address = UDPSocket::get_host_address("localhost", PORT);
@@ -24,38 +21,34 @@ void render_from_server()
     sprintf((char*)buffer, "This is packet");
     socket.send(send_address, buffer, strlen((char*)buffer));
 
+    World last_world, new_world;
+
     auto receive_world = std::chrono::high_resolution_clock::now();
+    auto last_receive_world = std::chrono::high_resolution_clock::now();
+    float millis_per_tick = 100.0f;
 
     while (!renderer.should_close_window()) {
         int message_len = 0;
         if (socket.receive(receive_address, buffer, MAX_BUFFER_LEN, message_len)) {
             last_world = new_world;
             new_world = World(buffer, message_len);
-            world_lerp = 0.0f;
 
+            last_receive_world = receive_world;
             receive_world = std::chrono::high_resolution_clock::now();
+            millis_per_tick = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(receive_world - last_receive_world).count());
         }
 
         auto this_frame = std::chrono::high_resolution_clock::now();
-        auto millis_since_update = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(this_frame - receive_world).count());
-        float world_lerp = millis_since_update / 100.0f; // (100 = ms per tick) TODO measure TPS from incoming packet rate
+        float millis_since_update = static_cast<float>(std::chrono::duration_cast<std::chrono::milliseconds>(this_frame - receive_world).count());
 
-           renderer.render(last_world.lerp_to(new_world, world_lerp));
-        //renderer.render(new_world);
+        auto inp = Input::read_state();
+        if (inp.clicking) {
+            renderer.render(last_world.lerp_to(new_world, millis_since_update / millis_per_tick));
+        } else {
+            renderer.render(new_world);
+        }
     }
-}
 
-void wait_for_char()
-{
-    using namespace std;
-    cout << "Enter a char to quit";
-    char x;
-    cin >> x;
-}
-
-int main(int argc, char** argv)
-{
-    render_from_server();
-//  wait_for_char();
+//  std::cout << "Enter a char to quit";char xxx; td::cin >> xxx;
     return 0;
 }
