5447afbeec8d972d364f0ec631877007804adacc
Jeremy Burns <j@jaburns.net>
2017-07-12 10:42:36 -0600

    Added some reference material

--- src/client/renderer/level.cpp
+++ src/client/renderer/level.cpp
@@ -39,6 +39,76 @@ LevelRenderer::~LevelRenderer()
     glDeleteBuffers(1, &m_index_buffer);
 }
 
+/*
+    Vector2[] insetPoints(Vector2[] points)
+    {
+        var ret = new Vector2[points.Length];
+
+        for (int i = 0; i < points.Length; i ++) {
+            var i0 = i == 0 ? points.Length - 1 : i - 1;
+            var i1 = i == points.Length - 1 ? 0 : i + 1;
+            Vector2 a = points[i0], b = points[i], c = points[i1];
+
+            var norm = ((b - a).normalized + (b - c).normalized).normalized;
+            if (((b - a).Cross(c - a) < 0) != targ.FlipNormals) norm *= -1;
+
+            ret[i] = b + norm * targ.SurfaceDepth;
+        }
+
+        return ret;
+    }
+
+    void bake()
+    {
+        var points = generatePoints();
+
+        var edge = targ.gameObject.EnsureComponent<EdgeCollider2D>();
+        edge.points = points.ToArray();
+
+        var originalVerts = points.ToArray();
+        var insetVerts = insetPoints(originalVerts);
+
+        var vertices = new Vector3[2 * originalVerts.Length];
+        var uvs = new Vector2[2 * originalVerts.Length];
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] = i % 2 == 0 ? originalVerts[i/2] : insetVerts[i/2];
+            uvs[i] = i % 2 == 0 ? Vector2.zero : Vector2.one;
+        }
+
+        int[] innerIndices = (new Triangulator(insetVerts)).Triangulate();
+
+        var indices = new int[3 * vertices.Length + innerIndices.Length];
+        for (int i = 0; i < vertices.Length; i += 2) {
+            var a = i;
+            var b = i+1;
+            var c = (i+2)%vertices.Length;
+            var d = (i+3)%vertices.Length;
+
+            indices[3*i  ] = targ.FlipNormals ? b : a;
+            indices[3*i+1] = targ.FlipNormals ? c : c;
+            indices[3*i+2] = targ.FlipNormals ? a : b;
+            indices[3*i+3] = targ.FlipNormals ? d : b;
+            indices[3*i+4] = targ.FlipNormals ? c : c;
+            indices[3*i+5] = targ.FlipNormals ? b : d;
+        }
+        for (int i = 0; i < innerIndices.Length; ++i) {
+            indices[3*vertices.Length + i] = 2*innerIndices[i] + 1;
+        }
+
+        var mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        var filter = targ.gameObject.EnsureComponent<MeshFilter>();
+        filter.sharedMesh = mesh;
+
+        targ.gameObject.EnsureComponent<MeshRenderer>();
+    }
+*/
+
 LevelRenderer::Mesh LevelRenderer::load_mesh(const BakedLevel& level)
 {
     LevelRenderer::Mesh mesh;
