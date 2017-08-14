d17a528032ce9a3fadb872ce28450ec7e72b68b1
Jeremy Burns <j@jaburns.net>
2017-08-14 09:53:10 -0600

    Added striped ground implicit texture to shader

--- src/client/palette.hpp
+++ src/client/palette.hpp
@@ -12,5 +12,5 @@ namespace Palette
     static constexpr glm::vec3 COLOR_SPACE = from_hex(0x2A1328);
     static constexpr glm::vec3 COLOR_DIRT  = { 0.054901f, 0.372549f, 0.462745f };
     static constexpr glm::vec3 COLOR_TURF  = { 0.031372f, 0.239215f, 0.337254f };
-    static constexpr glm::vec3 COLOR_LIFE  = { 0.843137f, 0.874509f, 0.443137f };
+    static constexpr glm::vec3 COLOR_LIFE  = from_hex(0xFFFFFF); // { 0.843137f, 0.874509f, 0.443137f };
 }
