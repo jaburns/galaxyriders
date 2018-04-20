dc0b7726b8f94b63e2ef94293d76295b9939e5b2
Jeremy Burns <j@jaburns.net>
2018-04-19 23:19:56 -0600

    Replaced shim header with copy of impl files for imgui support

--- external/imgui_support/GL/gl3w.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-// The ImGUI SDL+OpenGL example in their repository includes <GL/gl3w.h>
-// We just include our GL header here instead, so we can leave ImGUI unmodified.
-#include "../../../src/client/gl.hpp"
