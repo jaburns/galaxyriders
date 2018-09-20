1e82c55138e9e0a28c3a19dc5655e54901c11806
Jeremy Burns <j@jaburns.net>
2018-09-19 22:01:36 -0600

    Upped tick rate to 60 Hz

--- src/shared/config.hpp
+++ src/shared/config.hpp
@@ -4,7 +4,7 @@
 
 namespace Config
 {
-    static constexpr float MILLIS_PER_TICK = 33.333333f;
+    static constexpr float MILLIS_PER_TICK = 1000.0f / 60.0f;
 
     static constexpr uint32_t MAX_PACKET_SIZE = 512;
     static constexpr uint16_t DEFAULT_PORT = 12345;
